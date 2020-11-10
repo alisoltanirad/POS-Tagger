f4ffc28c9c356834a418da5ce0117ac48b3bd8f7
alisoltanirad <soltaniradali@gmail.com>
2020-11-10 14:40:37 +0330

    add tagger evaluation

--- pos_tagger.py
+++ pos_tagger.py
@@ -1,8 +1,16 @@
 import numpy as np
-from nltk.corpus import brown
+from nltk.corpus import brown, treebank
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from keras import Sequential
 from keras.layers import Dense, Flatten, LSTM
+
+
+def evaluate_tagger():
+    words, tags = get_corpus()
+    predicted_tags = pos_tag(words)
+    print('* POS tagger accuracy: {:.2%}'.format(accuracy_score(tags,
+                                                            predicted_tags)))
 
 
 def pos_tag(corpus):
@@ -12,7 +20,8 @@ def pos_tag(corpus):
 
 
 def train_tagger():
-    x_train, x_test, y_train, y_test = load_dataset()
+    x, y = load_dataset(brown)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
     model = compile_network()
     model.fit(x_train, y_train, validation_data=(x_test, y_test),
               batch_size=8, epochs=1)
@@ -34,21 +43,21 @@ def create_network():
     return network
 
 
-def load_dataset():
-    corpus = set(brown.tagged_words(tagset='universal'))
-    x = [word for (word, tag) in corpus]
-    y = [tag for (word, tag) in corpus]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
-    return x_train, x_test, y_train, y_test
+def load_dataset(corpus):
+    dataset = set(corpus.tagged_words(tagset='universal'))
+    x = [word for (word, tag) in dataset]
+    y = [tag for (word, tag) in dataset]
+    return x, y
 
 
 def get_corpus():
-    return
+    return load_dataset(treebank)
 
 
 def main():
-    corpus = get_corpus()
-    tagged_corpus = pos_tag(corpus)
+    words, true_tags = get_corpus()
+    predicted_tags = pos_tag(words)
+    evaluate_tagger()
 
 
 if __name__ == '__main__':
