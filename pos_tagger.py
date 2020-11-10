f0abf1fe30efa4ff0df829ae34498a6d9155b54f
alisoltanirad <soltaniradali@gmail.com>
2020-11-10 13:27:50 +0330

    initialize functions

--- pos_tagger.py
+++ pos_tagger.py
@@ -1,5 +1,22 @@
-def main():
+import numpy as np
+import nltk
+
+
+def pos_tag(corpus):
+    pos_tagger = train_tagger()
+
+
+def train_tagger():
     pass
+
+
+def get_corpus():
+    return
+
+
+def main():
+    corpus = get_corpus()
+    pos_tag(corpus)
 
 
 if __name__ == '__main__':
